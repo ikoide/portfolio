ec26cf84b49f476ed6370f721ddd35a6116b286a
lan <ian.koide@protonmail.com>
2020-03-29 01:11:27 -0400

    docs & title

--- portfolio/routes.py
+++ portfolio/routes.py
@@ -8,3 +8,7 @@ from portfolio import app, db, bcrypt
 @app.route("/")
 def home():
     return render_template("index.html", title='Home')
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html", title='Blog')
